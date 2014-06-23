921a16ebc3dded1e3c2f3ff5f13f07962d9523b1
Dimitrios Vlastaras <dimitrios@cryodev.se>
2014-06-23 15:37:47 +0200

    Truncated normal distibution fitting

---
+++ fitTruncNormDist.m
@@ -0,0 +1,22 @@
+function [ mu, sigma ] = fitTruncNormDist( truncData, truncPoint, side )
+%FITTRUNCNORMDIST Fitting of data to a truncated normal distribution
+%   This function takes a dataset and fits it to a truncated normal 
+%   distribution.
+
+    if strcmp(side,'right')
+        truncNormPdf = @(x,mu,sigma) normpdf(x,mu,sigma) ./ ...
+            normcdf(truncPoint,mu,sigma);
+        start = [mean(truncData),std(truncData)];
+        paramEsts = mle(truncData,'pdf',truncNormPdf,'start',start,...
+            'lower',[-Inf 0]);
+        mu = paramEsts(1);
+        sigma = paramEsts(2);
+    elseif strcmp(side,'left')
+        [mu,sigma] = fitTruncNormDist(-truncData,-truncPoint,'right');
+    else
+        mu = 0;
+        sigma = 0;
+        disp('The side has to be left or right')
+    end
+end
+
